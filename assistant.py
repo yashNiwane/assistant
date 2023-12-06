33d7e5d067a4e0a95c85efa399c6c323e1f8535e
Yash <120245614+yashNiwane@users.noreply.github.com>
2023-12-07 02:06:02 +0530

    Update assistant.py

--- assistant.py
+++ assistant.py
@@ -2,9 +2,14 @@ import pyttsx3
 import webbrowser
 import speech_recognition as sr
 import datetime
-import wikipediaapi
-import pyjokes
+import google.generativeai as palm
+import smtplib
 
+
+palm.configure(api_key='     *****Add your key heare*******      ')
+models = [m for m in palm.list_models() if 'generateText' in m.supported_generation_methods]
+model = models[0].name
+print(model)
 
 Assistant = pyttsx3.init()
 voices = Assistant.getProperty("voices")
@@ -14,12 +19,20 @@ def Speak(audio):
     print("   ")
     Assistant.say(audio)
     Assistant.runAndWait()
+    
+def sendEmail(to, subject, message):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login("teamgangamitra@gmail.com", "pvyy eheg jcbd dbku")
+    body = f"Subject: {subject}\n\n{message}"
+    server.sendmail("teamgangamitra@gmail.com", to, body)
+    server.quit()
 
 def takecommand():
     command = sr.Recognizer()
     with sr.Microphone() as source:
         print("Hearing....")
-        command.pause_threshold = 0.5  # Reduce the pause threshold for faster response
+        command.pause_threshold = 0.5 # Reduce the pause threshold for faster response
         audio = command.listen(source)
 
         try:
@@ -33,19 +46,7 @@ def takecommand():
             return "None"
         return query.lower()
 
-def search_wikipedia(query):
-    # Initialize the Wikipedia API
-    wiki_wiki = wikipediaapi.Wikipedia("en")
-    
-    # Use the API to search for the query
-    page = wiki_wiki.page(query)
-    
-    # Check if the page exists
-    if page.exists():
-        content = page.summary
-        Speak(content)
-    else:
-        Speak(f"Sorry, I couldn't find information about {query} on Wikipedia.")
+
 
 def TaskExe():
     while True:
@@ -53,6 +54,7 @@ def TaskExe():
 
         if "hello" in query:
             Speak("Hello! How can I assist you today?")
+            
         elif 'who are you' in query:
             Speak("I am your virtual assistant. What can I do for you?")
             
@@ -60,22 +62,14 @@ def TaskExe():
             query = query.replace("say hi to", "")
             Speak(f"hello {query}, how are you? ")
             
-        elif 'weather' in query:
-            Speak("I'm sorry, I don't have access to real-time weather data.")
-        elif 'joke' in query:
-            joke = pyjokes.get_joke()
-            Speak(joke)
         elif 'date' in query:
             current_date = datetime.datetime.now().strftime("%A, %d %B %Y")
             Speak(f"Today's date is {current_date}")
+            
         elif 'time' in query:
             current_time = datetime.datetime.now().strftime("%I:%M %p")
             Speak(f"The current time is {current_time}")
-        elif 'search for' in query:
-            query = query.replace("search for", "").strip()
-            search_wikipedia(query)
-        elif 'riddle' in query:
-            Speak("Sure, here's a riddle: What comes once in a minute, twice in a moment, but never in a thousand years?")
+        
         elif 'youtube search' in query:
             query = query.replace("youtube search", "")
             web = 'https://www.youtube.com/results?search_query=' + query
@@ -87,11 +81,38 @@ def TaskExe():
             web = 'https://www.google.com/search?q=' + query
             webbrowser.open(web)
             Speak('Done sir')
-
+            
+        elif 'send email' in query or "send mail" in query:
+            print("send E-mail to?")
+            Speak("send E-mail? to")
+            to = takecommand()
+            if "01" in to:
+             to = to.replace("01", "niwaneyash@gmail.com")
+            print(to)
+            print("What will be the subject of  Email?")
+            Speak("What will be the subject of  Email?")
+            subject = takecommand()
+            print("what will be the message?")
+            Speak("what will be the message?")
+            message = takecommand()
+            sendEmail(to, subject, message)
+            print("email send successfully")
+            Speak("email send successfully")
+        
         elif 'turn off' in query or 'bye' in query:
             Speak("Ok Sir, you can call me anytime!")
             break
+        
         else:
-            Speak("I'm sorry, I didn't understand your query. How can I assist you?")
-
+            prompt=query
+            response = palm.chat(messages=prompt)
+            response = palm.generate_text(
+            model=model,
+            prompt=prompt,
+            temperature=0,
+                     
+            max_output_tokens=100,
+)
+            Speak(response.result)
+            print(response.result)     
 TaskExe()
